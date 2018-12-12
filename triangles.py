0790945ae160b9bfd3824399f33afa60c9e763ce
Deniz <dzeran@ucvts.org>
2018-12-12 17:38:16 +0000

    added code to find hypotenuse

---
+++ triangles.py
@@ -0,0 +1,11 @@
+import math
+def pythageroem_theorem():
+    a = int(input("a = "))
+    b = int(input("b = "))
+
+
+    csqr = a*2 + b**2
+
+    c = math.sqrt(csqr)
+    print(f"c = {c}")
+pythageroem_theorem()
